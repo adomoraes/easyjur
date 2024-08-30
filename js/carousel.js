fad1296df250dd70d0b013899a1daea1f8b3388a
Eduardo Moraes <adomoraes@gmail.com>
2024-08-30 11:23:37 -0300

    feat: adicionar navegação com setas laterais ao carrossel de livros
    - Atualizado  para incluir botões de navegação laterais no carrossel de livros.
    - Modificado  para estilizar as setas de navegação, garantindo uma aparência consistente.
    - Ajustado  para implementar a lógica de navegação com setas, permitindo uma experiência de usuário mais intuitiva.

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
+	const $carouselWrapper = $(".book-carousel__wrapper")
+	const itemWidth = $(".book-carousel__item").outerWidth(true)
+
+	$(".book-carousel__nav--next").on("click", function () {
+		$carouselWrapper.animate(
+			{
+				scrollLeft: "+=" + itemWidth,
+			},
+			400
+		)
+	})
+
+	$(".book-carousel__nav--prev").on("click", function () {
+		$carouselWrapper.animate(
+			{
+				scrollLeft: "-=" + itemWidth,
+			},
+			400
+		)
+	})
+
 	$(".book-carousel__link").on("click", function (event) {
-		alert("Clicou!")
+		event.preventDefault()
+		const bookDetailUrl = $(this).attr("href")
+		window.location.href = bookDetailUrl
 	})
 })
